1bacbbad783832a024dbafc22a92bb1f3ba614b0
johnrj <johnrj@gmail.com>
2016-06-23 01:25:13 -0300

    adicionando dinamismo na tela de piadistas

--- PiadaRuim/app/src/main/java/br/com/matrixcorp/piadaruim/ListarPiadistasActivity.java
+++ PiadaRuim/app/src/main/java/br/com/matrixcorp/piadaruim/ListarPiadistasActivity.java
@@ -1,11 +1,14 @@
 package br.com.matrixcorp.piadaruim;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListarPiadistasActivity extends AppCompatActivity {
 
@@ -19,9 +22,17 @@ public class ListarPiadistasActivity extends AppCompatActivity {
 
         Bundle b = getIntent().getExtras();
 
-        String label = b.getBoolean("piadaBoa") ? getResources().getString(R.string.piadaBoa) : getResources().getString(R.string.piadaRuim);
-
-        ((TextView)findViewById(R.id.labeldaLista)).setText(label);
+        if(b.getBoolean("piadaBoa")) {
+            findViewById(R.id.avaliarPiada).setBackgroundColor(ContextCompat.getColor(this, R.color.blue));
+            ((Button)findViewById(R.id.avaliarPiada)).setText(getResources().getString(R.string.aplaudir));
+            setTitle(getResources().getString(R.string.piadaBoa));
+        }else {
+            findViewById(R.id.avaliarPiada).setBackgroundColor(ContextCompat.getColor(this, R.color.red));
+            ((Button)findViewById(R.id.avaliarPiada)).setText(getResources().getString(R.string.vaiar));
+            setTitle(getResources().getString(R.string.piadaRuim));
+        }
+        ((Button)findViewById(R.id.avaliarPiada)).setTextColor(ContextCompat.getColor(this, R.color.white));
+        ((Button)findViewById(R.id.avaliarPiada)).setPadding(5, 0, 5, 0);
 
         lista = (RadioGroup)findViewById(R.id.listaDepiadistas);
         listaDePiadistas = getResources().getStringArray(R.array.piadistas);
@@ -32,6 +43,31 @@ public class ListarPiadistasActivity extends AppCompatActivity {
             button.setId(i);
             button.setText(listaDePiadistas[i]);
             layout.addView(button);
+        }
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        lista = (RadioGroup)findViewById(R.id.listaDepiadistas);
+
+        if(lista.getCheckedRadioButtonId() != 0) {
+            Button btnDisplay = (Button) findViewById(R.id.avaliarPiada);
+
+            assert btnDisplay != null;
+            btnDisplay.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+
+                    int selectedId = lista.getCheckedRadioButtonId();
+
+                    RadioButton radio = (RadioButton) findViewById(selectedId);
+
+                    Toast.makeText(ListarPiadistasActivity.this, radio.getText(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
